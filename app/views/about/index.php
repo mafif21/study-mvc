2fbbf96414594359df7ff0b737a4515164a56a73
Muhammad Afif <afif.maliki21@gmail.com>
2022-09-27 00:18:19 +0700

    memodifikasi tampilang dengan bootsrap

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,2 +1,5 @@
-  <h1>Ini adalah halaman about index</h1>
-  <p>Halo nama saya <?= $data["name"] ?>, pekerjaan saya <?= $data["pekerjaan"] ?></p>
+  <div class="container">
+    <h1>Ini adalah halaman about index</h1>
+    <img src="<?= BASEURL ?>/img/test.jpg" alt="test" width="200" class="rounded-circle">
+    <p>Halo nama saya <?= $data["name"] ?>, pekerjaan saya <?= $data["pekerjaan"] ?></p>
+  </div>
--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -5,6 +5,24 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Halaman <?= $data['judul'] ?></title>
-  <link rel="stylesheet" href="http://localhost/phpmvc/public/css/bootstrap.css">
+  <link rel="stylesheet" href="<?= BASEURL; ?>/css/bootstrap.css">
 </head>
 <body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+      <a class="navbar-brand" href="<?= BASEURL ?>">PHPMVC</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="<?= BASEURL ?>">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="<?= BASEURL ?>/about">About</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
