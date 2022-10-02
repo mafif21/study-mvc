9978c9b5d7cc98f5dc9e34e9702f17b22f0593f4
Muhammad Afif <afif.maliki21@gmail.com>
2022-10-02 18:33:04 +0700

    manambahkan flash message ketika succes add data

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -1,6 +1,11 @@
 <div class="container">
   <div class="row">
-    <div class="col-6 mt-5">
+    <div class="col-lg-6">
+      <?php Flasher::flash(); ?>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-lg-6 mt-5">
     <button type="button" class="btn btn-primary mb-5" data-toggle="modal" data-target="#formmodal">
       Tambah Data Mahasiswa
     </button>
